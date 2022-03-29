30cdee8526af278bfc3ea47d84a6bbcd1fa938de
user.mail <artnumerique@yahoo.fr>
2022-03-29 22:18:02 +0200

    code source

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import trufoGif from "./trufo.gif"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <img src={trufoGif} alt="trufo gif" />
+     <h1> artcitéciné</h1>
+     <p>  "Lorsque j'étais critique, je pensais qu'un film, pour être réussi, doit exprimer simultanément une idée du monde et une idée du cinéma. La Règle du Jeu ou Citizen Kane répondaient bien à cette définition. Aujourd'hui je demande à un film d'exprimer soit la joie de faire du cinéma, soit l'angoisse de faire du cinéma et je me désintéresse de tout ce qui est entre les deux, c'est-à-dire de tous les films qui ne vibrent pas". </p>
     </div>
   );
 }
